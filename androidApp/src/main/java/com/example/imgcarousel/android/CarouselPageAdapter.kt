5cee36af6811fdb2e43f1a125b1cfad03f2c59cd
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-08-20 11:49:15 +0300

    Simple Carousel

---
+++ androidApp/src/main/java/com/example/imgcarousel/android/CarouselPageAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.imgcarousel.android
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
+
+class CarouselPageAdapter(
+    private val context: Context,
+    private val data: ArrayList<Int>,
+): RecyclerView.Adapter<CarouselPageAdapter.ViewHolder>(){
+
+    class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+       val Img: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.carousel_item, parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.Img.setImageResource(data[position])
+    }
+
+    override fun getItemCount() = data.size
+
+
+}
--- androidApp/src/main/java/com/example/imgcarousel/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/imgcarousel/android/MainActivity.kt
@@ -4,12 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.imgcarousel.Greeting
 import android.widget.TextView
+import androidx.constraintlayout.helper.widget.Carousel
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    var CarouselLayout: ConstraintLayout? = null
+    var viewPager: ViewPager2? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val Img: ArrayList<Int> = arrayListOf(R.drawable.cat1, R.drawable.cat2, R.drawable.cat3, R.drawable.cat4)
+
+        CarouselLayout = findViewById(R.id.Carousel)
+        viewPager = CarouselLayout?.findViewById(R.id.viewPager)
+        viewPager?.adapter = CarouselPageAdapter(this,Img)
+
     }
 }
-
