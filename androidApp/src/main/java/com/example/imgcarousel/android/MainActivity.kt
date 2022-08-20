a6c0789dc1632a3e5155cdb3cc21dff98d30e589
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-08-20 09:50:45 +0300

    Create Project

---
+++ androidApp/src/main/java/com/example/imgcarousel/android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.imgcarousel.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imgcarousel.Greeting
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
---
+++ shared/src/commonMain/kotlin/com/example/imgcarousel/Greeting.kt
@@ -0,0 +1,4 @@
+package com.example.imgcarousel
+
+class Greeting {
+}
